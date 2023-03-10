2c4b18c673ce0e56f8a5f43070802fb05f17d041
Oliver-EB <85533586+Oliver-EB@users.noreply.github.com>
2023-03-10 00:53:38 -0600

    Menu Dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { perfil} from './confperson/confp.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    perfil
   ],
   imports: [
     BrowserModule,
---
+++ src/app/confperson/confp.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'appcoonf',
+    templateUrl: './confp.component.html',
+    styleUrls: ['./confp.component.css']
+})
+export class perfil{
+    
+}
