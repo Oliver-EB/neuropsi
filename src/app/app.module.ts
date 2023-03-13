67986465b6728913123023b48fcb854cce27027e
AnaKaren1601 <anakaren160123@gmail.com>
2023-03-13 17:41:10 -0600

    Routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {RouterModule} from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { perfil} from './confperson/confp.component'
-import { appaciente } from './pacientes/paciente.component';
-import { appterapeuta } from './Terapeuta/terapeuta.component';
-import { appConsulta } from './Consulta/consulta.component';
-import { apprueba } from './pruebas/prueba.component';
 import { ConfigListComponent } from './config-list/config-list.component';
-import { ConsultaListComponent } from './consulta-list/consulta-list.component';
 import { PacientesListComponent } from './pacientes-list/pacientes-list.component';
-import { PruebasListComponent } from './pruebas-list/pruebas-list.component';
 import { TerapeutaListComponent } from './terapeuta-list/terapeuta-list.component';
+import { ConsultaListComponent } from './consulta-list/consulta-list.component';
+import { PruebasListComponent } from './pruebas-list/pruebas-list.component';
+
 @NgModule({
   declarations: [
     AppComponent,
-    perfil,
-    appaciente,
-    appterapeuta,
-    appConsulta,
-    apprueba,
     ConfigListComponent,
-    ConsultaListComponent,
     PacientesListComponent,
-    PruebasListComponent,
-    TerapeutaListComponent
+    TerapeutaListComponent,
+    ConsultaListComponent,
+    PruebasListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot([
+      {path: 'config-list', component: ConfigListComponent},
+      {path: 'pacientes-list', component: PacientesListComponent},
+      {path: 'terapeuta-list', component: TerapeutaListComponent},
+      {path: 'consulta-list', component: ConsultaListComponent},
+      {path: 'pruebas-list', component: PruebasListComponent},
+      {path: '', redirectTo: '/config-list', pathMatch: 'full'},
+      {path: '', redirectTo: '/pruebas-list', pathMatch: 'full'},
+      {path: '', redirectTo: '/consulta-list', pathMatch: 'full'},
+      {path: '', redirectTo: '/terapeuta-list', pathMatch: 'full'},
+      {path: '', redirectTo: '/pacientes-list', pathMatch: 'full'},
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
